7002b3e45f9f2f6f443d823529d0f1aa233467a9
Namra00 <namra.d.29@gmail.com>
2020-10-09 17:54:48 -0400

    Added new route shop from where we can access item collection and its preview

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
-
-const HatsPage = props => {
-  return(
-    <div>
-    <button onClick={() => props.history.push('/topics')}> Topics </button>
-    <h1>Hats Page</h1>
-  </div>
-  );
-};
-
-const TopicDetail = props => {
-  return (
-    <div>
-      <h1>TopicDetail: {props.match.params.topicId}</h1>
-    </div>
-  );
-};
-
-const TopicList = props => {
-  return (
-    <div>
-      <h1>TopicList</h1>
-      <Link to={`${props.match.url}/13`}>to topic 13</Link>
-    </div>
-  );
-};
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage}></Route>
-      <Route exact path='/hats/' component={HatsPage}></Route>
-      <Route exact path='/topics' component={TopicList}></Route>
-      <Route path='/topics/:topicId' component={TopicDetail}></Route>
+      <Route exact path='/shop' component={ShopPage}></Route>
     </div>
   );
 }
