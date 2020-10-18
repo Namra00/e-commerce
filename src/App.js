509c8d235f316aa2f3d8072c414ec0a0345f1318
Namra00 <namra.d.29@gmail.com>
2020-10-18 18:39:09 -0400

    Rendering User using Redux

--- src/App.js
+++ src/App.js
@@ -1,41 +1,34 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 
 class App extends React.Component{
-  constructor(){
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser ({
               id: snapShot.id,
               ...snapShot.data()
-            }
           });
-          // console.log(this.state);
-          
         });
       }
-      this.setState({ currentUser: userAuth });
+      setCurrentUser(userAuth);
     });
   }
 
@@ -46,7 +39,7 @@ class App extends React.Component{
   render(){
     return (
       <div>
-        <Header currentUser = { this.state.currentUser } />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage}></Route>
           <Route path='/shop' component={ShopPage}></Route>
@@ -58,4 +51,10 @@ class App extends React.Component{
   
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './header.styles.scss';
 
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { ReactComponent as Logo } from './../../assets/crown.svg';
 import { auth } from './../../firebase/firebase.utils';
 
@@ -16,12 +17,18 @@ const header = ({ currentUser }) =>(
             <Link className='option' to='/contact' > CONTACT </Link>
             {
                 currentUser ?
-                <div className='option' onClick = {() => auth.signOut()}> SIGN OUT </div>
+               ( <div className='option' onClick = {() => auth.signOut()}> SIGN OUT </div>)
                 :
-                <Link className='option' to='/signin'> SIGN IN </Link>
+                (<Link className='option' to='/signin'> SIGN IN </Link>)
             }
         </div>
     </div>
 );
 
-export default header;
+
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+
+export default connect(mapStateToProps)(header);
